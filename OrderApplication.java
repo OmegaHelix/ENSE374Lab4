89b08e2ac1655d5c475c30d72b780fc715b032ec
OmegaHelix <dakotaf@sasktel.net>
2017-10-25 16:13:01 -0600

    began OrderApplication Class
    Gave the class a catalogue variable. Implemented declarations of methods.

---
+++ OrderApplication.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class OrderApplication
+{
+    private ArrayList<Product> catalogue = new ArrayList<Product>();
+    
+
+    private Boolean importCatalogue(){
+       
+        return true;
+    }
+
+    public Boolean createOrder(Order order){
+
+    }
+
+}
