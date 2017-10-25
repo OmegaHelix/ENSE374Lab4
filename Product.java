dea00be49f1c7c7fbd7d12c7da23d3e5afb9a14a
OmegaHelix <dakotaf@sasktel.net>
2017-10-25 16:28:03 -0600

    created Product class and basic methods

---
+++ Product.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+
+public class Product{
+
+    private String name = new String();
+    private Double price;
+    private Int productID;
+
+    public String getName()
+    {
+        return this.name;
+    }
+
+    public Double getPrice()
+    {
+        return this.price;
+    }
+
+    public Int getProductID()
+    {
+        return this.productID;
+    }
+
+}
