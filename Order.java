c493e804082b146d1a825746034c3dd780e36241
OmegaHelix <dakotaf@sasktel.net>
2017-10-25 16:19:42 -0600

    created interface and basic instantiation of

---
+++ Order.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Order {
+
+    private Date dateRecieved;
+    private Double price;
+    private Int orderID;
+    private ArrayList<OrderLine> cart = new ArrayList<OrderLine>();
+    
+
+    public Date getDateRecieved(){}
+    public Double calculatePrice(){}
+
+}
