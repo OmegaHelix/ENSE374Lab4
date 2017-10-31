befe6c6fcf66d0f51e9a37e3860501a25dd22a9a
OmegaHelix <dakotaf@sasktel.net>
2017-10-30 20:29:43 -0600

    added CorporateCustomer class with basic methods.

---
+++ CoporateCustomer.java
@@ -0,0 +1,17 @@
+
+public class CorporateCustomer{
+
+    private String contactName;
+
+    public void setContactName(String contactName)
+    {
+        this.contactName = contactName;
+    }
+
+    public String getContactName()
+    {
+        return this.contactName;
+    }
+
+    
+}
