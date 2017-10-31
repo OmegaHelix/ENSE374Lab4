2642d0cf42c6747a15448046bfad5053d64afc4c
OmegaHelix <dakotaf@sasktel.net>
2017-10-30 20:33:09 -0600

    added PersonalCustomer class with basic methods.

---
+++ PersonalCustomer.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+
+public class PersonalCustomer{
+    private Int creditCardNumber;
+
+    public Int getCreditCardNumber()
+    {
+        return this.creditCardNumber;
+    }
+
+    public void setCreditCardNumber(Int creditCardNumber)
+    {
+        this.creditCardNumber = creditCardNumber;
+    }
+
+}
