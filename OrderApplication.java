9ed6ae5a30599ce9ac4037f4c9f78996834b9f6a
OmegaHelix <dakotaf@sasktel.net>
2017-10-30 21:09:55 -0600

    Added importCatalogue method

--- OrderApplication.java
+++ OrderApplication.java
@@ -8,6 +8,26 @@ public class OrderApplication
 
     private Boolean importCatalogue(){
        
+        this.catalogue = new ArrayList<Product>();
+		String line = new String();
+		try {
+			Scanner fileIn = new Scanner(Paths.get("catalog.cvs"));
+			if(fileIn.hasNextLine())
+			{
+				line = fileIn.nextLine(); // skips header line
+			}
+			while (fileIn.hasNextLine()) {
+				line = fileIn.nextLine();
+				employees.add(new Product(line));
+			}
+			fileIn.close();
+		
+		}
+		 
+		catch (IOException e) {
+            e.printStackTrace();
+            return false;
+		}
         return true;
     }
 
